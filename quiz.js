a382f6997766aa1cfdfd33f329ed7e16cab379d5
SSVsharma <103274101+SSVsharma@users.noreply.github.com>
2023-09-29 15:00:54 +0530

    Create quiz.js

---
+++ quiz.js
@@ -0,0 +1,213 @@
+const quizData = [
+    {
+      question: 'Identify the noun in " I live in India."',
+      options: ['I', 'in', 'India', 'Love'],
+      answer: 'India',
+    },
+    {
+      question: 'Identify the verb in "Please open the door.".',
+      options: ['Please', 'open', 'the', 'none'],
+      answer: 'open',
+    },
+    {
+      question: 'The product of 9×2 is?',
+      options: ['11', '17', '18', '7'],
+      answer: '18',
+    },
+    {
+      question: 'The sum of 21+6+4?',
+      options: ['21', '10', '25', '31'],
+      answer: '31',
+    },
+    {
+      question: 'Which is the largest ocean on Earth?',
+      options: [
+        'Pacific Ocean',
+        'Indian Ocean',
+        'Atlantic Ocean',
+        'Arctic Ocean',
+      ],
+      answer: 'Pacific Ocean',
+    },
+    {
+      question: '7 fours are ?',
+      options: ['88', '48', '38', '28'],
+      answer: '28',
+    },
+    {
+      question: 'Who painted the Mona Lisa?',
+      options: [
+        'Pablo Picasso',
+        'Vincent van Gogh',
+        'Leonardo da Vinci',
+        'Michelangelo',
+      ],
+      answer: 'Leonardo da Vinci',
+    },
+    {
+      question: 'Identify the adjective in "Ria lives in a beautiful house".',
+      options: ['house', 'beautiful', 'Ria', 'lives'],
+      answer: 'beautiful',
+    },
+    {
+      question: 'Identify the synonym of Listen?',
+      options: [
+         'Hear','quite','play','talk'
+      ],
+      answer: 'Hear',
+    },
+    {
+      question: 'Which animal is known as the King of the Jungle?',
+      options: ['Lion', 'Tiger', 'Elephant', 'Giraffe'],
+      answer: 'Lion',
+    },
+    {
+      question: 'Identify the tense in "He plays basketball every day.".',
+      options: ['Present', 'Past', 'Future', 'Past perfect'],
+      answer: 'present',
+    },
+    {
+      question: 'What is the next prime number after 5?',
+      options: ['3', '7', '10', '11'],
+      answer: '7',
+    },
+    {
+      question: 'Choose The Right Spelling : It is _______.',
+      options: ['wondrfull', 'wendorfull', 'wonderful', 'wanderfool'],
+      answer: 'wonderful',
+    },
+    {
+      question: 'Which of these have more tens?',
+      options: ['355', '769', '199', '832'],
+      answer: '832',
+    },
+    {
+      question: 'Taj Mahal is located in?',
+      options: ['Delhi', 'Mumbai', 'Agra', 'Bengaluru'],
+      answer: 'Agra',
+    },
+  ];
+  
+  const quizContainer = document.getElementById('quiz');
+  const resultContainer = document.getElementById('result');
+  const submitButton = document.getElementById('submit');
+  const retryButton = document.getElementById('retry');
+  const showAnswerButton = document.getElementById('showAnswer');
+  
+  let currentQuestion = 0;
+  let score = 0;
+  let incorrectAnswers = [];
+  
+  function shuffleArray(array) {
+    for (let i = array.length - 1; i > 0; i--) {
+      const j = Math.floor(Math.random() * (i + 1));
+      [array[i], array[j]] = [array[j], array[i]];
+    }
+  }
+  
+  function displayQuestion() {
+    const questionData = quizData[currentQuestion];
+  
+    const questionElement = document.createElement('div');
+    questionElement.className = 'question';
+    questionElement.innerHTML = questionData.question;
+  
+    const optionsElement = document.createElement('div');
+    optionsElement.className = 'options';
+  
+    const shuffledOptions = [...questionData.options];
+    shuffleArray(shuffledOptions);
+  
+    for (let i = 0; i < shuffledOptions.length; i++) {
+      const option = document.createElement('label');
+      option.className = 'option';
+  
+      const radio = document.createElement('input');
+      radio.type = 'radio';
+      radio.name = 'quiz';
+      radio.value = shuffledOptions[i];
+  
+      const optionText = document.createTextNode(shuffledOptions[i]);
+  
+      option.appendChild(radio);
+      option.appendChild(optionText);
+      optionsElement.appendChild(option);
+    }
+  
+    quizContainer.innerHTML = '';
+    quizContainer.appendChild(questionElement);
+    quizContainer.appendChild(optionsElement);
+  }
+  
+  function checkAnswer() {
+    const selectedOption = document.querySelector('input[name="quiz"]:checked');
+    if (selectedOption) {
+      const answer = selectedOption.value;
+      if (answer === quizData[currentQuestion].answer) {
+        score++;
+      } else {
+        incorrectAnswers.push({
+          question: quizData[currentQuestion].question,
+          incorrectAnswer: answer,
+          correctAnswer: quizData[currentQuestion].answer,
+        });
+      }
+      currentQuestion++;
+      selectedOption.checked = false;
+      if (currentQuestion < quizData.length) {
+        displayQuestion();
+      } else {
+        displayResult();
+      }
+    }
+  }
+  
+  function displayResult() {
+    quizContainer.style.display = 'none';
+    submitButton.style.display = 'none';
+    retryButton.style.display = 'inline-block';
+    showAnswerButton.style.display = 'inline-block';
+    resultContainer.innerHTML = `You scored ${score} out of ${quizData.length}!`;
+  }
+  
+  function retryQuiz() {
+    currentQuestion = 0;
+    score = 0;
+    incorrectAnswers = [];
+    quizContainer.style.display = 'block';
+    submitButton.style.display = 'inline-block';
+    retryButton.style.display = 'none';
+    showAnswerButton.style.display = 'none';
+    resultContainer.innerHTML = '';
+    displayQuestion();
+  }
+  
+  function showAnswer() {
+    quizContainer.style.display = 'none';
+    submitButton.style.display = 'none';
+    retryButton.style.display = 'inline-block';
+    showAnswerButton.style.display = 'none';
+  
+    let incorrectAnswersHtml = '';
+    for (let i = 0; i < incorrectAnswers.length; i++) {
+      incorrectAnswersHtml += `
+        <p>
+          <strong>Question:</strong> ${incorrectAnswers[i].question}<br>
+          <strong>Your Answer:</strong> ${incorrectAnswers[i].incorrectAnswer}<br>
+          <strong>Correct Answer:</strong> ${incorrectAnswers[i].correctAnswer}
+        </p>
+      `;
+    }
+  
+    resultContainer.innerHTML = `
+      <p>You scored ${score} out of ${quizData.length}!</p>
+      <p>Incorrect Answers:</p>
+      ${incorrectAnswersHtml}
+    `;
+  }
+  
+  submitButton.addEventListener('click', checkAnswer);
+  retryButton.addEventListener('click', retryQuiz);
+  showAnswerButton.addEventListener('click', showAnswer);
+  
+  displayQuestion();
