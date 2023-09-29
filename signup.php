35d66a383b0726ab5693c78256fe29837a1884a9
SSVsharma <103274101+SSVsharma@users.noreply.github.com>
2023-09-29 21:24:58 +0530

    Update signup.php

--- signup.php
+++ signup.php
@@ -3,7 +3,7 @@ $name=$_POST['username'];
 $email=$_POST['email'];
 $password=$_POST['password'];
 
-$conn=new mysqli('sups','root','','registers');
+$conn=new mysqli('abcd','root','','registers');
 if($conn->connect_error){
     die('Connection Failed:'.$conn->connect_error);
 }
